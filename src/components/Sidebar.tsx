c544b4ec735b860b16872bf49b3868af3ad06782
Raul789 <raul.66.turk@gmail.com>
2025-02-17 11:03:33 +0200

    Phase 1.1

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,20 @@
+import JobList from "./JobList";
+import PaginationControls from "./PaginationControls";
+import ResultsCount from "./ResultsCount";
+import SortingConstrols from "./SortingControls";
+
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top"></div>
+      <div className="sidebar__top">
+        <ResultsCount/>
+        <SortingConstrols/>
+
+      </div>
+
+      <JobList/>
+      
+      <PaginationControls/>
     </div>
   );
 }
